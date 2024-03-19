6299c9ff810f6541b83fa09a602baeccfc3eab51
SMKGui <guii0305@outlook.com>
2024-03-19 14:25:07 -0300

    Notifications

--- vitalHub/src/Components/CancelationModal/CancelationModal.js
+++ vitalHub/src/Components/CancelationModal/CancelationModal.js
@@ -3,12 +3,37 @@ import { Title } from "../Title/Style"
 import { ButtonSecondaryTitle, ButtonTitle } from "../ButtonTitle/Style"
 import { ModalContent, ModalText, PatientModal } from "./Style"
 import { ButtonModal, ButtonSecondary } from "../Button/Style"
+import * as Notifications from 'expo-notifications';
+
+
 
 export const CancelationModal = ({
     visible,
     setShowModalCancel,
     ...rest
 }) => {
+
+    //funcao para lidar com a chamada de notificacao
+  const handleCallNotifications = async () => {
+
+    //obtem o status da permissao
+    const { status } = await Notifications.getPermissionsAsync();
+
+    //verifica se o usuario concedeu permissão
+    if (status !== "granted") {
+      alert("voce nao deixou as notificacoes ativas")
+      return;
+    }
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Consulta cancelada",
+        body: "Notificação recebida."
+      },
+      trigger: null
+    })
+
+  }
     return(
         <Modal 
         {...rest} 
@@ -27,7 +52,7 @@ export const CancelationModal = ({
                     </ModalText>
 
                     {/* button */}
-                    <ButtonModal>
+                    <ButtonModal onPress={handleCallNotifications}>
                         <ButtonTitle>Confirmar</ButtonTitle>
                     </ButtonModal>
 
